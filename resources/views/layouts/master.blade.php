9e9cef00b05a1093a01a6692c88cc748aae2e037
BugaPunk <punksbuga@outlook.es>
2023-12-05 14:55:18 -0400

    [ADD] nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,6 +17,9 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <x-nav-link href="{{ route('/categorias') }}" :active="request()->routeIs('/categorias')">
+                {{ __('Categorias') }}
+            </x-nav-link>
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{}}">Inicio</a>
               </li>
